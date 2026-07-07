23425d5cfdbbc26cf8be1d467ee861689d0fbd07
Natã Junior <natajuniorpinheirodasilva@gmail.com>
2026-07-07 03:53:34 -0300

    add Dashboard as a parent component

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { useMemo, useState } from "react"
+import { Transaction } from "@/lib/types"
+import { groupByCategory, groupByMonth } from "@/lib/aggregations"
+import Transactions from "./Transactions"
+import CategoryPieChart from "./charts/CategoryPieChart"
+
+const Dashboard = () => {
+    const [transactionState, setTransactionState] = useState<Transaction[]>([])
+    
+    const handleAdd = (newTransaction: Transaction) => { setTransactionState([ newTransaction, ...transactionState ]) }
+
+    const cachedCategoryValue = useMemo(() => groupByCategory(transactionState), [transactionState])
+    const cachedMonthlyValue = useMemo(() => groupByMonth(transactionState), [transactionState] )
+
+    return (
+        <div>
+            <Transactions onAdd={handleAdd} transactions={transactionState}/>
+            <CategoryPieChart data={cachedCategoryValue}/>
+        </div>
+    )
+}
+
+export default Dashboard
